3859d9e948bcac07ea2c9eb69710afa749aa5c4c
BryanBradfo <bryanchen1105@gmail.com>
2024-06-19 10:15:33 +0800

    add of utils

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,6 +2,28 @@ import torch
 import numpy as np
 from src.decompression.decompress import restoreLinearLayer
 from src.compression.LowRankLinear import generate_rank
+from src.utils.criterions import LabelSmoothingCrossEntropyLoss
+
+# def evaluate_accuracy_gpu(model, test_ds):
+#     """
+#     @param model : PyTorch model to evaluate accuracy.
+#     @param test_ds : Test dataset.
+
+#     @return model accuracy.
+#     """
+#     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+#     model.eval() # Set to eval state.
+#     t_correct, t_dp = 0, 0
+#     with torch.no_grad():
+#         for i, label in test_ds:
+#             _, opt = torch.max(model(i.to(device)), dim = 1)
+#             t_dp += label.size(0)
+#             t_correct += (opt == label.to(device)).sum().item()
+#     acc = t_correct / t_dp
+#     print("model accuracy: {}".format(acc))
+#     model.train() # Revert to training state.
+#     return acc
 
 def evaluate_accuracy_gpu(model, test_ds):
     """
@@ -13,16 +35,20 @@ def evaluate_accuracy_gpu(model, test_ds):
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     model.eval() # Set to eval state.
-    t_correct, t_dp = 0, 0
+    valid_loss = 0
+    valid_acc = 0
     with torch.no_grad():
         for i, label in test_ds:
-            _, opt = torch.max(model(i.to(device)), dim = 1)
-            t_dp += label.size(0)
-            t_correct += (opt == label.to(device)).sum().item()
-    acc = t_correct / t_dp
-    print("model accuracy: {}".format(acc))
+            output = model(i.to(device))
+            loss = LabelSmoothingCrossEntropyLoss(classes=10)(output, label.to(device))
+            valid_loss += loss.item() * i.size(0)
+            valid_acc = torch.eq(output.argmax(-1), label.to(device)).float().mean()
+    
+    valid_loss /= len(test_ds.dataset)
+
+    print("model accuracy: {}".format(valid_acc))
     model.train() # Revert to training state.
-    return acc
+    return valid_acc, valid_loss
 
 def evaluate_accuracy(model, test_ds):
     """
