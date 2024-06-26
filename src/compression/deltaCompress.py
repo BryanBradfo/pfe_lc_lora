00b3ec64fe9878ffa6ade91b9ce429e4413cc0e9
BryanBradfo <bryanchen1105@gmail.com>
2024-06-26 15:44:37 +0800

    add of delta compression

--- src/compression/deltaCompress.py
+++ src/compression/deltaCompress.py
@@ -26,7 +26,52 @@ def generate_decomposed_names(original_layers):
         
     return decomposed_layers
 
-def compress_data(δt, num_bits = 3, threshhold=True):
+# def compress_data(δt, num_bits = 3, threshhold=True):
+#     """
+#     @param δt : The delta to compress.
+#     @param num_bits : The number of bits to limit huffman encoded variables to.
+#     @param treshold : Enabler for priority promotion process.
+
+#     @return Zlib compressed promoted delta and uncompressed version.
+#     """
+#     _, δt_exp = np.frexp(δt)
+#     δt_sign = np.sign(δt)
+#     δt_sign[δt_sign > 0] = 0
+#     δt_sign[δt_sign < 0] = 1    
+#     mp =  defaultdict(list)
+#     for i in range(len(δt)):
+#         mp[(δt_exp[i], δt_sign[i])].append((i, δt[i]))
+#     for k in mp:
+#         mp[k] = (np.average(np.array([x[-1] for x in mp[k]])), 
+#                  [x[0] for x in mp[k]])
+#     mp = list(mp.values())
+#     # print("len(mp):", len(mp))
+#     if threshhold:
+#         allowed_buckets = int(math.pow(2, num_bits)-1)
+#         # print("allowed_buckets:", allowed_buckets)
+#         # print("min between len(mp) and allowed_buckets:", min(allowed_buckets, len(mp)))
+#         mp = sorted(mp, key = lambda x : abs(x[0]), reverse = True)[:min(allowed_buckets, len(mp))]
+#         # print("len(mp):", len(mp))
+#     new_δt= [0 for x in range(len(δt))]
+#     for qtVal, pos in mp:
+#         for p in pos:
+#             new_δt[p] = qtVal
+#     new_δt = np.array(new_δt, dtype = np.float32)
+#     # Compress the data
+#     # compressed_data = zlib.compress(new_δt)
+#     # Get the size of the original data
+#     # original_size = len(new_δt)
+#     # Get the size of the compressed data
+#     # compressed_size = len(compressed_data)
+#     # Calculate the compression ratio
+#     # compression_ratio = original_size / compressed_size
+#     # print("from src")
+#     # print("Original size:", original_size, "bytes")
+#     # print("Compressed size:", compressed_size, "bytes")
+#     # print("Compression ratio:", compression_ratio)
+#     return zlib.compress(new_δt), new_δt
+
+def compress_data(δt, num_bits=3, threshhold=True):
     """
     @param δt : The delta to compress.
     @param num_bits : The number of bits to limit huffman encoded variables to.
@@ -34,39 +79,28 @@ def compress_data(δt, num_bits = 3, threshhold=True):
 
     @return Zlib compressed promoted delta and uncompressed version.
     """
+    # Get exponents and signs
     _, δt_exp = np.frexp(δt)
-    δt_sign = np.sign(δt)
-    δt_sign[δt_sign > 0] = 0
-    δt_sign[δt_sign < 0] = 1    
-    mp =  defaultdict(list)
-    for i in range(len(δt)):
-        mp[(δt_exp[i], δt_sign[i])].append((i, δt[i]))
-    for k in mp:
-        mp[k] = (np.average(np.array([x[-1] for x in mp[k]])), 
-                 [x[0] for x in mp[k]])
-    mp = list(mp.values())
-    # print("len(mp):", len(mp))
+    δt_sign = (np.sign(δt) < 0).astype(int)
+
+    # Group by (exponent, sign)
+    groups = defaultdict(list)
+    for value, exp, sign in zip(δt, δt_exp, δt_sign):
+        groups[(exp, sign)].append(value)
+
+    # Calculate averages
+    bucket_list = [(np.mean(values), np.array(indices)) for (indices, values) in groups.items()]
+
     if threshhold:
-        allowed_buckets = int(math.pow(2, num_bits)-1)
-        # print("allowed_buckets:", allowed_buckets)
-        # print("min between len(mp) and allowed_buckets:", min(allowed_buckets, len(mp)))
-        mp = sorted(mp, key = lambda x : abs(x[0]), reverse = True)[:min(allowed_buckets, len(mp))]
-        # print("len(mp):", len(mp))
-    new_δt= [0 for x in range(len(δt))]
-    for qtVal, pos in mp:
-        for p in pos:
-            new_δt[p] = qtVal
-    new_δt = np.array(new_δt, dtype = np.float32)
-    # Compress the data
-    compressed_data = zlib.compress(new_δt)
-    # Get the size of the original data
-    # original_size = len(new_δt)
-    # Get the size of the compressed data
-    # compressed_size = len(compressed_data)
-    # Calculate the compression ratio
-    # compression_ratio = original_size / compressed_size
-    # print("from src")
-    # print("Original size:", original_size, "bytes")
-    # print("Compressed size:", compressed_size, "bytes")
-    # print("Compression ratio:", compression_ratio)
-    return zlib.compress(new_δt), new_δt
+        allowed_buckets = (1 << num_bits) - 1
+        bucket_list = sorted(bucket_list, key=lambda x: abs(x[0]), reverse=True)[:allowed_buckets]
+
+    # Create new delta
+    new_δt = np.zeros_like(δt, dtype=np.float32)
+    for val, indices in bucket_list:
+        new_δt[indices] = val
+
+    # Compress the new delta
+    compressed_δt = zlib.compress(new_δt)
+
+    return compressed_δt, new_δt
