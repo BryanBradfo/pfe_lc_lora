8942e85b260b10562623bd5f03b9a7c5b6cc9899
BryanBradfo <bryanchen1105@gmail.com>
2024-07-19 16:56:12 +0800

    add of utils

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -24,6 +24,30 @@ def evaluate_accuracy_gpu(model, test_ds, device):
     model.train() # Revert to training state.
     return acc
 
+def evaluate_accuracy_vit_gpu(model, test_ds, device):
+    """
+    @param model : PyTorch model to evaluate accuracy.
+    @param test_ds : Test dataset.
+
+    @return model accuracy.
+    """
+    
+    model.eval()  # Set to eval state.
+    t_correct, t_dp = 0, 0
+    with torch.no_grad():
+        for i, label in test_ds:
+            i, label = i.to(device), label.to(device)
+            opt = model(i)
+            # Extract the logits from ImageClassifierOutput
+            logits = opt.logits if hasattr(opt, 'logits') else opt
+            # Assuming logits is the tensor containing model predictions
+            predicted = torch.argmax(logits, dim=1)
+            t_dp += label.size(0)
+            t_correct += (predicted == label).sum().item()
+    acc = t_correct / t_dp
+    model.train()  # Revert to training state.
+    return acc
+
 def evaluate_accuracy(model, test_ds):
     """
     @param model : PyTorch model to evaluate accuracy.
@@ -38,6 +62,27 @@ def evaluate_accuracy(model, test_ds):
             _, opt = torch.max(model(i), dim = 1)
             t_dp += label.size(0)
             t_correct += (opt == label).sum().item()
+    acc = t_correct / t_dp
+    # print("model accuracy: {}".format(acc))
+    model.train() # Revert to training state.
+    return acc
+
+def evaluate_accuracy_vit(model, test_ds):
+    """
+    @param model : PyTorch model to evaluate accuracy.
+    @param test_ds : Test dataset.
+
+    @return model accuracy.
+    """
+    model.eval() # Set to eval state.
+    t_correct, t_dp = 0, 0
+    with torch.no_grad():
+        for i, label in test_ds:
+            opt = model(i)
+            logits = opt.logits if hasattr(opt, 'logits') else opt
+            predicted = torch.argmax(logits, dim=1)
+            t_dp += label.size(0)
+            t_correct += (predicted == label).sum().item()
     acc = t_correct / t_dp
     # print("model accuracy: {}".format(acc))
     model.train() # Revert to training state.
@@ -105,7 +150,6 @@ def lazy_restore_gpu(weights, weights_decomp, bias, clean_model, org, decomposed
     
     @return lazily restored model (restoration with weights vector settled) for evaluating checkpoint accuracy.
     """
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
     base_dict = clean_model.state_dict()
     last_idx, last_idx_dcomp = 0, 0
@@ -140,41 +184,6 @@ def lazy_restore_gpu(weights, weights_decomp, bias, clean_model, org, decomposed
             last_idx += t_elements
     clean_model.load_state_dict(base_dict)
     return clean_model
-    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-    # base_dict = clean_model.state_dict()
-    # last_idx, last_idx_dcomp = 0, 0
-    # for layer_name, init_tensor in base_dict.items():
-    #     if "bias" in layer_name:
-    #         base_dict[layer_name] = bias[layer_name]
-    #         continue
-    #     dim = init_tensor.cpu().detach().numpy().shape
-    #     if not dim:
-    #         continue
-    #     if layer_name in decomposed_layers: # Restoration procedure for dense layers.
-    #         if rank == -1:
-    #             rr = generate_rank(dim[0], dim[1])
-    #         else:
-    #             rr = rank
-    #         t_element_alpha = dim[0] * rr
-    #         t_element_beta = dim[1] * rr
-    #         alpha = weights_decomp[last_idx_dcomp : last_idx_dcomp + t_element_alpha]
-    #         last_idx_dcomp += t_element_alpha
-    #         beta = weights_decomp[last_idx_dcomp : last_idx_dcomp + t_element_beta]
-    #         last_idx_dcomp += t_element_beta
-    #         alpha = torch.unflatten(torch.from_numpy(np.copy(alpha)), -1, (dim[0], rr))
-    #         beta = torch.unflatten(torch.from_numpy(np.copy(beta)), -1, (rr, dim[1]))
-    #         restored_decomp = restoreLinearLayer(alpha.to(device), beta.to(device), org[layer_name], scaling)
-    #         base_dict[layer_name] = restored_decomp
-    #     elif "classifier" in layer_name:
-    #         base_dict[layer_name] = bias[layer_name]
-    #     else: # Restoration procedure for convolutional layers.
-    #         t_elements = np.prod(dim)
-    #         needed_ele = weights[last_idx : last_idx + t_elements]
-    #         base_dict[layer_name] = torch.unflatten(torch.from_numpy(np.copy(needed_ele)), -1, dim)
-    #         last_idx += t_elements
-    # clean_model.load_state_dict(base_dict)
-    # return clean_model
 
 def lazy_restore(weights, weights_decomp, bias, clean_model, org, decomposed_layers, rank : int = -1, scaling : int = -1):
     """
